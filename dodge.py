bda68e7ec050907fa8614ca4c825676e43f2c6bb
Mike <119834037+pstlo@users.noreply.github.com>
2023-01-11 01:08:26 -0500

    Add files via upload
    -Reworked levels, now easier to add more

--- dodge.py
+++ dodge.py
@@ -6,16 +6,19 @@ from os import environ
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
 pygame.init()
+pygame.mouse.set_visible(False)
 
 # GAME CONSTANTS
-    
-# SCREEN
-screenSize = [800,800] # Default = [800,800]
-screenColor = [0,0,0] # Default = [0,0,0]
-fps = 60 # Default = 60
-timerSize = 75 # Default = 100
-timerColor = [255,255,255] # Default = [255,255,255]
+#------------------------------------------------------------------------------------------------------------------
+# SCREEN                                                                                                                            
+screenSize = [800,800] # Default = [800,800]                                  
+screenColor = [0,0,0] # Default = [0,0,0]                                                                
+fps = 60 # Default = 60                                    
+timerSize = 75 # Default = 100                             
+timerColor = [255,255,255] # Default = [255,255,255] 
 timerDelay = 1000 # Default = 1000
+levelSize = 50 # Default = 50                                                                                                                                                                   
+levelColor = [255,255,255] # Default = [255,255,255]                                                                                                                                            
 
 # PLAYER
 playerColor = [255,255,255] # Default = [255,255,255]
@@ -30,31 +33,68 @@ obstacleColor = [255,0,0] # Default = [255,0,0]
 maxObstacles = 12  # Default = 12
 obstacleBoundaries = "KILL" # Default = "KILL" (Can be updated by level)
 
-# LEVELS
-levelColor = [255,255,255]
-currentLevel = 1
-levelSize = 25
-# starts at levelOne
+# LEVELS:   [ False , TIME, BOUNDS, SPEED, SIZE, NUMBER, COLOR ] , then add to levelSettingsList
+levelTwo = [ False, 15, "KILL", 1, 1, 1.5, [255,255,0] ] # Default = [ False, 15, "KILL", 1, 1, 1.5, [255,255,0] ]
+levelThree = [False, 30, "KILL", 1, 1.5, 1, [0,0,255] ] # Default = [False, 30, "KILL", 1, 1.5, 1, [0,0,255] ]
+levelFour = [False, 45, "KILL", 1, 1.5, 1, [0,255,0] ] # Default = [False, 45, "KILL", 1, 1.5, 1, [0,255,0] ]
+levelFive = [False, 60, "KILL", 1.25, 1, 1, [0,255,255] ] # Default = [False, 60, "KILL", 1.25, 1, 1, [0,255,255] ]
+levelSix = [False, 75, "WRAP", 0.5, 10, 0.1, [255,0,255] ] # Default = [False, 75, "WRAP", 0.5, 10, 0.1, [255,0,255] ]
+levelSeven = [False, 90, "KILL", 2, 0.1, 10, [100,0,50] ] # Default = [False, 90, "KILL", 2, 0.1, 10, [100,0,50] ]
+levelEight = [False, 105, "BOUNCE", 1.25, 1.25, 1.0, [0,150,50] ] # Default = [False, 105, "BOUNCE", 1.25, 1.25, 1.0, [0,150,50] ]
+levelSettingsList = [levelTwo,levelThree,levelFour,levelFive,levelSix,levelSeven,levelEight] # Defaults = [levelTwo,levelThree,levelFour,levelFive,levelSix,levelSeven,levelEight]
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+class Player(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.color = playerColor 
+            self.size = playerSize        
+            self.pos = [screenSize[0] / 2 , screenSize[1] / 2]
+            self.speed = playerSpeed              
+            self.growth = playerGrowth            
+            
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.color = obstacleColor
+        self.speed = obstacleSpeed
+        self.size = obstacleSize
+        self.movement = getMovement()
+        self.pos = [self.movement[0][0], self.movement[0][1]]
+        self.direction = self.movement[1]
 
-levelTwo = [False,"KILL",2]
-levelThree = [False,"KILL",3]
-levelFour = [False,"KILL",4]
-levelFive = [False,"KILL",5]
-levelSix = [False,"KILL",6]
-levelSeven = [False,"BOUNCE",7]
-levelEight =[False,"KILL",8]
-# INITIALIZE LEVELS 
+currentLevel = 1    
+player = Player()
+obstacles = []
+clk = pygame.time.Clock()
+screen = pygame.display.set_mode(screenSize)
+gameClock = 0        
+        
+def levelDictSetter(levelSettingsList):
+    levelDictList = []
+    for settings in levelSettingsList:
+        levelDict = {
+                        "START" : settings[0], 
+                        "TIME" : settings[1],
+                        "bound" : settings[2],  
+                        "speedMult" : settings[3],
+                        "obsSizeMult" : settings[4],
+                        "maxObsMult" : settings[5],
+                        "COLOR" :  settings[6]                    
+                    }              
+        levelDictList.append(levelDict)   
+    return levelDictList
 
-def main():
-    
-    def getPosition():
+levelDictList = levelDictSetter(levelSettingsList)
+
+
+def getMovement():
         X = random.randint(0, screenSize[0])
         Y = random.randint(0, screenSize[1])
 
-        lowerX = random.randint(0, screenSize[0] * 0.1)
-        upperX =  random.randint(screenSize[0] * 0.9, screenSize[0])
-        lowerY  = random.randint(0, screenSize[1] * 0.1)
-        upperY = random.randint(screenSize[1] * 0.9, screenSize[1])
+        lowerX = random.randint(0, screenSize[0] * 0.05)
+        upperX =  random.randint(screenSize[0] * 0.95, screenSize[0])
+        lowerY  = random.randint(0, screenSize[1] * 0.05)
+        upperY = random.randint(screenSize[1] * 0.95, screenSize[1])
         
         topDir = ["S", "E", "W", "SE", "SW"]
         leftDir = ["E", "S", "N", "NE", "SE"]
@@ -72,18 +112,14 @@ def main():
         rightBound = [upperX, Y, rightDirection]
      
         possible = [topBound, leftBound, rightBound, bottomBound]
-        selected = possible[ random.randint(0, len(possible) - 1) ]
+        movement = possible[ random.randint(0, len(possible) - 1) ]
         
-        return selected
-
-    def getMovement():
-        movement = getPosition()
         position = [movement[0], movement[1]]
         direction = movement[2]
         move = [position,direction]
-        return move
-        
-    def movementReverse(direction):
+        return move 
+ 
+def movementReverse(direction):
         if direction == "N":
             return "S"
         elif direction == "S":
@@ -99,37 +135,13 @@ def main():
         elif direction == "SE":
             return "NW"
         elif direction == "SW":
-            return "NE"
-            
-                    
-    class Player(pygame.sprite.Sprite):
-        def __init__(self):
-            pygame.sprite.Sprite.__init__(self)
-            self.color = playerColor 
-            self.size = playerSize        
-            self.pos = [screenSize[0] / 2 , screenSize[1] / 2]
-            self.speed = playerSpeed              
-            self.growth = playerGrowth            
-            
-    class Obstacle(pygame.sprite.Sprite):
-        def __init__(self):
-            pygame.sprite.Sprite.__init__(self)
-            self.color = obstacleColor
-            self.speed = obstacleSpeed
-            self.size = obstacleSize
-            self.movement = getMovement()
-            self.pos = [self.movement[0][0], self.movement[0][1]]
-            self.direction = self.movement[1]
-            self.level = currentLevel
-            
+            return "NE"     
+              
+def main():
     
     # GAME LOGIC
-    player = Player()
-    obstacles = []
-    clk = pygame.time.Clock()
-    screen = pygame.display.set_mode(screenSize)
     
-    gameClock = 0
+    global gameClock
     
     # TIMER DISPLAY
     timerFont = pygame.font.SysFont(None, timerSize)
@@ -137,13 +149,7 @@ def main():
     timerEvent = pygame.USEREVENT + 1
     pygame.time.set_timer(timerEvent, timerDelay)
     
-
-
-    
-    global running
     running = True
-    
-
     
     def movement():
         
@@ -202,8 +208,7 @@ def main():
                 
             if "W" in obs.direction:
                 obs.pos[0] -= obs.speed
-                
-
+   
                 
     def obstacleRemove():
         for obs in obstacles:
@@ -250,86 +255,26 @@ def main():
                 obs.pos[0] = screenSize[0]
     
     
-    def levels():
-        # Variables that get updated by levels are declared globally below
-        global obstacleBoundaries,maxObstacles,obstacleSize,obstacleColor,obstacleSpeed
-        
-        # Level start initialized globally below
-        global currentLevel,levelTwo,levelThree,levelFour,levelFive,levelSix,levelSeven,levelEight
-        
-        # STARTS ON LEVEL 1 
-        
-        # LEVEL 2
-        if gameClock == 15 and levelTwo[0] == False:
-            maxObstacles *= 1.5
-            obstacleColor = [255,255,0]
-            obstacleBoundaries = levelTwo[1]
-            currentLevel +=1
-            levelTwo[0] = True
-        
-        # LEVEL 3
-        elif gameClock == 30 and levelThree[0] == False:
-            obstacleSize *= 1.5
-            obstacleColor = [0,0,255]
-            currentLevel +=1
-            obstacleBoundaries = levelThree[1]
-            levelThree[0] = True
-        
-        # LEVEL 4
-        elif gameClock == 45 and not levelFour[0]:
-            obstacleSize *= 1.5
-            obstacleColor = [0,255,0]
-            currentLevel +=1
-            obstacleBoundaries = levelFour[1]
-            levelFour[0] = True
-        
-        # LEVEL 5
-        elif gameClock == 60 and not levelFive[0]:
-            obstacleSpeed *= 1.25
-            obstacleColor = [0,255,255]
-            currentLevel +=1
-            obstacleBoundaries = levelFive[1]
-            levelFive[0] = True
-        
-        # LEVEL 6
-        elif gameClock == 75 and not levelSix[0]:
-            obstacleSize *= 10
-            obstacleSpeed /= 2
-            maxObstacles /= 10
-            obstacleColor = [255,0,255]
-            currentLevel +=1
-            obstacleBoundaries = levelSix[1]
-            levelSix[0] = True
-        
-        # LEVEL 7
-        elif gameClock == 90 and not levelSeven[0]:
-            obstacleSize /= 10
-            maxObstacles *= 1.5
-            obstacleSpeed *= 1.25
-            obstacleColor = [100, 0, 50]
-            currentLevel +=1
-            obstacleBoundaries = levelSeven[1]
-            levelSeven[0] = True
-            
-        # LEVEL 8
-        elif gameClock == 105 and not levelEight[0]:
-            obstacleSize *= 1.5
-            obstacleColor = [0,150,0]
-            currentLevel +=1
-            obstacleBoundaries = levelEight[1]
-            levelEight[0] = True
-        
-                        
-    def update():
-        pygame.draw.circle(screen, player.color, player.pos, player.size)
-        timerRect = timerDisplay.get_rect(topright = screen.get_rect().topright)
-        levelRect = levelDisplay.get_rect(topleft = screen.get_rect().topleft)
-        screen.blit(timerDisplay, timerRect)
-        screen.blit(levelDisplay, levelRect)
-        pygame.display.flip()
-        screen.fill(screenColor)
-        clk.tick(fps)
-        
+    def levelDictSelector(levelDictList):
+        for levelDict in levelDictList:
+            if levelDict["TIME"] == gameClock:
+                return levelDict
+
+    def levelUpdater(levelDict):   # PASS THE CORRESPONDING DICT TO THIS FUNCTION
+        global obstacleBoundaries, obstacleSpeed, obstacleColor, maxObstacles, obstacleSize, currentLevel
+    
+        try:
+            if not levelDict["START"] and levelDict["TIME"] == gameClock:
+                    obstacleBoundaries = levelDict["bound"]
+                    obstacleSpeed *= levelDict["speedMult"]
+                    obstacleColor = levelDict["COLOR"]
+                    maxObstacles *= levelDict["maxObsMult"]
+                    obstacleSize *= levelDict["obsSizeMult"]
+                    currentLevel += 1
+                    levelDict["START"] = True
+        except:
+            pass
+
     # GAME LOOP
     while running:
         
@@ -353,10 +298,7 @@ def main():
             obsCenter = obs.pos
             if ( (math.dist(playerCenter,obsCenter)) < (player.size + obs.size) ):
                 running = False
-                pygame.quit()
-                sys.exit()
-                
-        levels()    
+                    
         movement()
         wrapping()
         spawner()
@@ -371,8 +313,19 @@ def main():
         if obstacleBoundaries == "WRAP":
             wrapObstacle()
             
+        levelDict = levelDictSelector(levelDictList)
+        
+        levelUpdater(levelDict)    
+                
         drawObstacles()    
-        update()
+        pygame.draw.circle(screen, player.color, player.pos, player.size)
+        timerRect = timerDisplay.get_rect(topright = screen.get_rect().topright)
+        levelRect = levelDisplay.get_rect(topleft = screen.get_rect().topleft)
+        screen.blit(timerDisplay, timerRect)
+        screen.blit(levelDisplay, levelRect)
+        pygame.display.flip()
+        screen.fill(screenColor)
+        clk.tick(fps)
         
 if __name__ == '__main__':
     main()
