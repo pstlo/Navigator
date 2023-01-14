d040c0d13f098d13d2edaa663a25fe6f983e4198
Mike <119834037+pstlo@users.noreply.github.com>
2023-01-13 22:18:56 -0500

    Added Stage 2

--- Navigator/Navigator.py
+++ Navigator/Navigator.py
@@ -47,9 +47,9 @@ obstacleBoundaries = "KILL" # Default = "KILL" (Can be updated by level)
 # LEVELS  
 levelTimer = 15 # Default = 15 / Time between levels
 
-speedIncrement = obstacleSpeed / 15 # Default = obstacleSpeed / 15
-sizeIncrement = round(obstacleSize/7) # Default = round(obstacleSize/7)
-numIncrement = maxObstacles / 4 # Default = maxObstacles / 4
+speedIncrement = obstacleSpeed / 16 # Default = obstacleSpeed / 15
+sizeIncrement = round(obstacleSize/8) # Default = round(obstacleSize/7)
+numIncrement = maxObstacles / 6 # Default = maxObstacles / 4
 
 # [ False , (levelNumber - 1) * levelTimer , BOUNDS, SPEED, SIZE, NUMBER ]
 levelTwo = [ False, levelTimer, "KILL", speedIncrement, sizeIncrement, numIncrement ] 
@@ -61,11 +61,14 @@ levelSeven = [ False, 6 * levelTimer, "KILL", speedIncrement, sizeIncrement, num
 levelEight = [ False, 7 * levelTimer, "BOUNCE", speedIncrement, sizeIncrement, numIncrement ] 
 levelNine = [ False, 8 * levelTimer, "KILL", speedIncrement, sizeIncrement, numIncrement ]
 levelTen = [ False, 9 * levelTimer, "WRAP", speedIncrement, sizeIncrement, numIncrement ]
-overTime = [ False, 10 * levelTimer, "KILL", speedIncrement, sizeIncrement * 2, - numIncrement/2 ]
+
+overTimeOne = [ False, 10 * levelTimer, "KILL", speedIncrement, sizeIncrement * 2, - numIncrement/2 ]
+overTimeTwo = [ False, 11 * levelTimer, "KILL", speedIncrement, sizeIncrement, numIncrement ]
+overTimeThree = [ False, 12 * levelTimer, "KILL", speedIncrement, sizeIncrement, numIncrement ]
 
 # DIVIDE INTO STAGES
-stageOneLevels = [levelTwo,levelThree,levelFour,levelFive,levelSix,levelSeven,levelEight,levelNine,levelTen,overTime] # Stage 1
-overTimeLevels = [overTime]
+stageOneLevels = [levelTwo,levelThree,levelFour,levelFive,levelSix,levelSeven,levelEight,levelNine,levelTen] # Stage 1
+overTimeLevels = [overTimeOne,overTimeTwo,overTimeThree]
 
 # STORE IN LIST
 stageList = [stageOneLevels, overTimeLevels] # List of stages
