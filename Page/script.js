61e268e5664a5d6d049f2a7490a10e35e7eaf947
Mike <119834037+pstlo@users.noreply.github.com>
2023-04-27 18:44:19 -0400

    Add files via upload

---
+++ Page/script.js
@@ -0,0 +1,41 @@
+// Navigator page backend
+
+// Switch between pages
+function switchPage(page) {
+    var homePage = document.getElementById("home");
+    var downloadsPage = document.getElementById("downloads");
+    var supportPage = document.getElementById("support");
+
+    if (page === "home") {
+        homePage.style.display = "flex";
+		homePage.style.flexDirection = "column";
+        downloadsPage.style.display = "none";
+        supportPage.style.display = "none";
+    } else if (page === "downloads") {
+        homePage.style.display = "none";
+        downloadsPage.style.display = "block";
+        supportPage.style.display = "none";
+    } else if (page === "support") {
+        homePage.style.display = "none";
+        downloadsPage.style.display = "none";
+        supportPage.style.display = "flex";
+		supportPage.style.flexDirection = "column";
+    }
+}
+
+// Animate
+var img = document.getElementById("img");
+var x = 0;
+var direction = 1;
+var speed = 5;
+
+function moveImage() {
+	x += direction * speed;
+	img.style.left = x + "px";
+
+	if (x >= window.innerWidth/2 || x <= window.innerWidth/-2) {
+		direction *= -1;
+	}
+}
+
+setInterval(moveImage, 10);
