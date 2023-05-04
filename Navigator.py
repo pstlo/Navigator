aece39b05ee21b9f61e007de12d6c26e3b3abd4f
Mike <119834037+pstlo@users.noreply.github.com>
2023-05-04 03:40:07 -0400

    Update Navigator.py

--- Navigator.py
+++ Navigator.py
@@ -1,7 +1,7 @@
 # Navigator
 # Copyright (c) 2023 Mike Pistolesi
 # All rights reserved
- 
+
 
 import os,sys,random,math,platform,json,base64,time,pypresence,asyncio
 from cryptography.fernet import Fernet
