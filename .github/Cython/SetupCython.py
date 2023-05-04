d3c842d1a0786d3018cfe671599407fd572f2e4d
Mike <119834037+pstlo@users.noreply.github.com>
2023-05-04 03:11:07 -0400

    Add files via upload

---
+++ .github/Cython/SetupCython.py
@@ -0,0 +1,12 @@
+# Setup for Cython conversion
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+extensions = [Extension("Navigator", ["Navigator.py"],)]
+
+setup(
+    name='Navigator',
+    version='0.4.7',
+    author='Mike Pistolesi',
+    ext_modules=cythonize(extensions, language_level=3),
+    install_requires=['pygame', 'cryptography', 'python-dotenv', 'pypresence'])
