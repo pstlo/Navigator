80da717767981af558fc38ab84051b681d861091
Mike <119834037+pstlo@users.noreply.github.com>
2023-05-17 22:14:20 -0400

    Delete CreateExeC.bat

--- .github/CreateExeC.bat
+++
@@ -1,1 +0,0 @@
-pyinstaller --name Navigator --onefile --noconsole --add-data ..\..\Assets;Assets --add-data "Navigator.cp310-win_amd64.pyd;." -i="..\Icons\Icon.ico" --version-file ..\version.txt Main.py
