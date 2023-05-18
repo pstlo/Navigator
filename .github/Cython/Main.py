da332cb8e0aa3c083a421b90630ca903be806105
Mike <119834037+pstlo@users.noreply.github.com>
2023-05-17 22:17:13 -0400

    Add files via upload

--- .github/Cython/Main.py
+++ .github/Cython/Main.py
@@ -1,10 +1,8 @@
-# Run Navigator from PYD file
 import os,sys,random,math,platform,json,base64,time,pypresence,asyncio
 from cryptography.fernet import Fernet
 from dotenv import load_dotenv
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
-sys.path.append('.')
 import Navigator
 if __name__ == "__main__": Navigator.gameLoop()
 
