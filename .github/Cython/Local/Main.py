99ea28d34251497a425908eeffd5d3b462bfbe27
Mike <119834037+pstlo@users.noreply.github.com>
2023-05-22 23:04:04 -0400

    Add files via upload

--- .github/Cython/Local/Main.py
+++ .github/Cython/Local/Main.py
@@ -1,7 +1,8 @@
 # Run Navigator from PYD file on local machine ( replace the parent directory's "main.py" file )
-import os,sys,random,math,platform,json,base64,time,pypresence,asyncio
+import os,sys,random,math,platform,json,base64,time,pypresence,asyncio,dns
 from cryptography.fernet import Fernet
 from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
 sys.path.append('.')
