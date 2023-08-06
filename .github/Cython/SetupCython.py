3ad097b65e3533aa7aba7a2e016dfa8825d44d14
Mike <119834037+pstlo@users.noreply.github.com>
2023-08-06 06:15:34 -0400

    Add files via upload

--- .github/Cython/SetupCython.py
+++ .github/Cython/SetupCython.py
@@ -1,12 +1,12 @@
-# Setup for Cython conversion from github action
+# Setup for Cython conversion on local machine ( replace the parent directory's "SetupCython.py" file )
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
-extensions = [Extension("Navigator", ["Navigator.py"],)]
+extensions = [Extension("Navigator", ["../../Navigator.py"],)]
 
 setup(
     name='Navigator',
-    version='0.4.9',
+    version='0.5.0',
     author='Mike Pistolesi',
     ext_modules=cythonize(extensions, language_level=3),
     install_requires=['pygame','cryptography','python-dotenv','pypresence','dnspython','certifi','pymongo==3.11'])
