5ba5ca146b9feba6e7d17d5041fc3ef711bf8a26
Mike <119834037+pstlo@users.noreply.github.com>
2023-11-10 00:47:29 -0500

    Update CreateExeC.bat

--- .github/Cython/CreateExeC.bat
+++ .github/Cython/CreateExeC.bat
@@ -1,1 +1,1 @@
-pyinstaller --name Navigator --onefile --noconsole --add-data ..\..\Assets;Assets --add-data "Navigator.cp310-win_amd64.pyd;." -i="..\Icons\Icon.ico" --version-file ..\version.txt Main.py
+pyinstaller --name Navigator --onefile --noconsole --add-data ..\..\Assets;Assets --add-data "Navigator.cp312-win_amd64.pyd;." -i="..\Icons\Icon.ico" --version-file ..\version.txt Main.py
