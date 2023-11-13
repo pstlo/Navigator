b3151f54da479207e0c72bb723512924dc4f9f1e
Mike <119834037+pstlo@users.noreply.github.com>
2023-11-12 20:22:41 -0500

    Add files via upload

--- .github/Cython/CreateAppC.command
+++ .github/Cython/CreateAppC.command
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-pyinstaller -y --name=Navigator --noconsole --windowed --add-data "../Assets":Assets --add-data "../Navigator.cpython-311-darwin.so":. -i=Icon.icns "Main.py"
+pyinstaller -y --name=Navigator --noconsole --windowed --add-data "../../Navigator/Assets":Assets --add-data *.so:. -i=Icon.icns "cMain.py"
--- .github/Cython/CreateExeC.bat
+++ .github/Cython/CreateExeC.bat
@@ -1,1 +1,1 @@
-pyinstaller --name Navigator --onefile --noconsole --add-data ..\..\Assets;Assets --add-data "Navigator.cp312-win_amd64.pyd;." -i="..\Icons\Icon.ico" --version-file ..\version.txt Main.py
+pyinstaller --name Navigator --onefile --noconsole --add-data ..\..\Navigator\Assets;Assets --add-data *.pyd;. -i="..\Icons\Icon.ico" --version-file ..\version.txt cMain.py
