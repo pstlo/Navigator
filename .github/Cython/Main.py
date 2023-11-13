e5579dedbea1da3ad9133ac0f6d4ab540c88729a
Mike <119834037+pstlo@users.noreply.github.com>
2023-11-12 20:26:30 -0500

    Add files via upload

--- .github/Cython/Main.py
+++ .github/Cython/Main.py
@@ -1,10 +1,10 @@
-# For github action
+# Run Navigator from PYD file on local machine 
 import os,sys,random,math,platform,json,base64,time,pypresence,asyncio,dns,certifi
 from cryptography.fernet import Fernet
 from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
-import Navigator
-if __name__ == "__main__": Navigator.game.gameLoop()
+from Navigator import *
+game.gameLoop()
 
