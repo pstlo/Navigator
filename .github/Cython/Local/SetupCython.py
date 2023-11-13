fe6686478c4113246bae344fed145b44ccfe7968
Mike <119834037+pstlo@users.noreply.github.com>
2023-11-12 20:18:39 -0500

    Add files via upload

--- .github/Cython/Local/SetupCython.py
+++ .github/Cython/Local/SetupCython.py
@@ -2,11 +2,26 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
-extensions = [Extension("Navigator", ["../../Navigator/Navigator.py"],)]
+extensions = [Extension("Navigator", ["../../Navigator/Navigator.py"]), 
+        Extension("Settings",["../../Navigator/Settings.py"]),
+        Extension("Game",["../../Navigator/Game.py"]),
+        Extension("Assets",["../../Navigator/Assets.py"]),
+        Extension("Event",["../../Navigator/Event.py"]),
+        Extension("Explosion",["../../Navigator/Explosion.py"]),
+        Extension("Gamepad",["../../Navigator/Gamepad.py"]),
+        Extension("Lasers",["../../Navigator/Lasers.py"]),
+        Extension("Menu",["../../Navigator/Menu.py"]),
+        Extension("Obstacles",["../../Navigator/Obstacles.py"]),
+        Extension("Player",["../../Navigator/Player.py"]),
+        Extension("Point",["../../Navigator/Point.py"]),
+        Extension("Presence",["../../Navigator/Presence.py"]),
+        Extension("Unlocks",["../../Navigator/Unlocks.py"])
+    ]
+    
 
 setup(
     name='Navigator',
-    version='0.5.0',
+    version='0.5.1',
     author='Mike Pistolesi',
     ext_modules=cythonize(extensions, language_level=3),
     install_requires=['pygame','cryptography','python-dotenv','pypresence','dnspython','certifi','pymongo==3.11'])
