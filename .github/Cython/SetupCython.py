ae3fbf39610264675470b0f01ecdb3c4c848b76c
Mike <119834037+pstlo@users.noreply.github.com>
2023-11-12 20:30:53 -0500

    Add files via upload

--- .github/Cython/SetupCython.py
+++ .github/Cython/SetupCython.py
@@ -1,21 +1,21 @@
-# Setup for Cython conversion on local machine ( replace the parent directory's "SetupCython.py" file )
+# Setup for automated cython conversion
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
-extensions = [Extension("Navigator", ["../../Navigator/Navigator.py"]), 
-        Extension("Settings",["../../Navigator/Settings.py"]),
-        Extension("Game",["../../Navigator/Game.py"]),
-        Extension("Assets",["../../Navigator/Assets.py"]),
-        Extension("Event",["../../Navigator/Event.py"]),
-        Extension("Explosion",["../../Navigator/Explosion.py"]),
-        Extension("Gamepad",["../../Navigator/Gamepad.py"]),
-        Extension("Lasers",["../../Navigator/Lasers.py"]),
-        Extension("Menu",["../../Navigator/Menu.py"]),
-        Extension("Obstacles",["../../Navigator/Obstacles.py"]),
-        Extension("Player",["../../Navigator/Player.py"]),
-        Extension("Point",["../../Navigator/Point.py"]),
-        Extension("Presence",["../../Navigator/Presence.py"]),
-        Extension("Unlocks",["../../Navigator/Unlocks.py"])
+extensions = [Extension("Navigator", ["Navigator/Navigator.py"]), 
+        Extension("Settings",["Navigator/Settings.py"]),
+        Extension("Game",["Navigator/Game.py"]),
+        Extension("Assets",["Navigator/Assets.py"]),
+        Extension("Event",["Navigator/Event.py"]),
+        Extension("Explosion",["Navigator/Explosion.py"]),
+        Extension("Gamepad",["Navigator/Gamepad.py"]),
+        Extension("Lasers",["Navigator/Lasers.py"]),
+        Extension("Menu",["Navigator/Menu.py"]),
+        Extension("Obstacles",["Navigator/Obstacles.py"]),
+        Extension("Player",["Navigator/Player.py"]),
+        Extension("Point",["Navigator/Point.py"]),
+        Extension("Presence",["Navigator/Presence.py"]),
+        Extension("Unlocks",["Navigator/Unlocks.py"])
     ]
     
 
